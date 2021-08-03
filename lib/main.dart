c7de1713528e3f219161c91e42901103ca775aaf
fiki <fiki@pop-os.localdomain>
2021-08-03 21:49:49 +0700

    add counter app

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  // method untuk penambahan
+  void _increment(){
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // method untuk pengurangan
+  void _decrement(){
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Counter App")
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_counter.toString(), style: TextStyle(fontSize: 10 + _counter.toDouble()),),
+              SizedBox(height: 5,),
+              ElevatedButton(
+                onPressed: () {
+                  _increment();
+                },
+                child: Text('Increment')),
+                ElevatedButton(
+                onPressed: () {
+                  _decrement();
+                },
+                child: Text('Decrement'))
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
